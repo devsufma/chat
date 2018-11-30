645048a215705af65713927552825bf7ad82e918
Eduardo <euodeionomedeusuario@gmail.com>
2018-11-30 19:24:12 -0300

    changing interface

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('connection', function(username, room){
+    console.log(username + " is connected.");
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(username, msg, room){
     console.log('message: ' + msg);
   });
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(username, msg, room){
+    io.emit('chat message', username, msg, room);
   });
 });
 
